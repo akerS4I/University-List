faca36e25424ff0627fedcc150fa76f67be9b73a
AKER <recrentic@gmail.com>
2024-09-15 14:52:28 +0500

    added data

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 console.log("app.js connected");
+
+const container = document.getElementById("container");
+
+fetch("../data/tashkent_universities.json")
+ .then((response) => response.json())
+ .then((data) => {
+  console.log(data);
+  // Access the data
+ })
+ .catch((error) => console.error("Error loading JSON:", error));
+
+fillList();
+
+function fillList(code) {
+ container.innerHtml = code;
+}
