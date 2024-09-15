37dfed362503f1490d0e5b24e74031db658ce95e
AKER <recrentic@gmail.com>
2024-09-15 18:12:07 +0500

    bug fix

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ console.log("app.js connected");
 
 const container = document.getElementById("container");
 
-fetch("../data/tashkent_universities.json")
+fetch("./data/tashkent_universities.json")
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
@@ -46,33 +46,4 @@ function fillList(list) {
   htmlString = htmlString.replace(/,/g, "");
 
   container.innerHTML = htmlString;
-}
-
-{
-  /* <div class="card">
-<div class="desc">
-  <p>Tashkent State University of Economics</p>
-  <br />
-  City: Tashkent
-  <br />
-  Type: Public
-  <br />
-  <br />
-  
-  <div class="bold">Number: <span>+998 71 233 10 70</span></div>
-  Address: 78 University Street, Tashkent, Uzbekistan
-</div>
-<div class="socials">
-  <a href="https://www.facebook.com/economics.uz"
-    ><button><i class="fa-brands fa-facebook"></i></button
-  ></a>
-  <a href="https://twitter.com/economics_uz"
-    ><button><i class="fa-brands fa-twitter"></i></button
-  ></a>
-  <a
-    href="https://www.linkedin.com/school/tashkent-state-university-of-economics/"
-    ><button><i class="fa-brands fa-linkedin"></i></button
-  ></a>
-</div>
-</div> */
 }
